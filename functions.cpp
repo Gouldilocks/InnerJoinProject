a274ec58d41683bad7bde591bfc2966e0087de3e
Gouldilocks <Gouldilocks@users.noreply.github.com>
2020-03-02 22:12:36 -0600

    made program mostly, still issues with id's not matching outputs

--- functions.cpp
+++ functions.cpp
@@ -1,19 +1,74 @@
+#include <iostream>
 #include "functions.h"
+#include <vector>
+#include <sstream>
 
-vector<vector<string>> readTable(ifstream &file) {
-    /**
-    ** Implement readTable here
-    **/
+using namespace std;
+
+//given a file, read the file and put it into a 2 dimensional vector.
+vector<vector<string>> readTable (ifstream &file) {
+	//all the variables that will be used.
+	string line; // the string that will be used to fill all the vectors.
+	vector<vector<string>> returnMe; // vector to be sent back to main.
+	vector<string> vectorOfLine; // the vector of each of the lines.
+
+	//this while loop will end when it has finished getting all the lines in the file.
+	while (getline (file, line)) {   // get a whole line
+		stringstream ss (line);//make it a string stream so that we can use getline()
+		//this while loop will end when it has finished organizing the whole line
+		while (getline (ss, line, ',')) {
+			vectorOfLine.push_back (line);
+		}
+		returnMe.push_back (vectorOfLine);//put the newly found vector of a line into the returning vector.
+		vectorOfLine.resize (0);//resize the vector so that the next line values are not stored here.
+	}
+	return returnMe;
 }
 
-void writeTable(ofstream &file, vector<vector<string>> &table) {
-    /**
-    ** Implement writeTable here
-    **/
+void writeTable (ofstream &file, vector<vector<string>> &table) {
+	for (int i = 0; i < table.size (); i++) {
+		cout << table.at (i).at (0);
+		for (int x = 1; x < table.at (i).size (); x++) {
+			cout << "," << table.at (i).at (x);
+		}
+		cout << endl;
+	}
 }
+vector<vector<string>> innerJoin (vector<vector<string>> leftTable, vector<vector<string>> rightTable) {
+	string idToMatch = "";
+//for (int i = 0; i < leftTable.size(); i ++){ // for each left table vector, do this.
+//	idToMatch = leftTable.at(i).at(0); // get the id to match from the
+//	for (int x = 0; x < rightTable.size(); x ++ ){ //for each right table vector, do this.
+//		if (rightTable.at(x).at(0).compare(idToMatch) == 0){ // if the ids are the same, then do this.
+//			for (int p = 1; p < rightTable.at(x).size(); p++){ // for each value in this id# (except 0), do this.
+//				leftTable.at(i).push_back(rightTable.at(x).at(p));
+//			}
+//		} else continue; // if the strings are not the same, keep going through the ids.
+//	}
+//}
+	vector <vector<string>> returnMe(leftTable.size ());
+	vector <string> commons;
+	for (int z = 0; z < rightTable.size (); z++) {
+		for (int i = 0; i < leftTable.size (); i++) {
+			if (leftTable.at (i).at(0) == rightTable.at(z).at(0)){
+				commons.push_back(rightTable.at(z).at(0));
+			}
+		}
+	}
+	for (int i = 0; i < commons.size (); i++){
+		returnMe.at(i).push_back(commons.at(i));
+	}
+	for (int x = 1; x < leftTable.at(0).size(); x++) {
+		for (int i = 0; i < leftTable.size (); i++) { // this loop makes all the ids in the returnMe vector.
+			returnMe.at (i).push_back (leftTable.at (i).at (x));
+		}
+	}
+	for (int x = 1; x < rightTable.at(0).size(); x++) {
+		for (int i = 0; i < rightTable.size (); i++) { // this loop makes all the ids in the returnMe vector.
+			returnMe.at (i).push_back (rightTable.at (i).at (x));
+		}
+	}
 
-vector<vector<string>> innerJoin(vector<vector<string>> leftTable, vector<vector<string>> rightTable) {
-    /**
-    ** Implement innerJoin here
-    **/
+	return returnMe;
+
 }
--- main.cpp
+++ main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 #include "functions.h"
 
-int main() {
-    std::ofstream file("../Results.csv");
+using namespace std;
 
-    file << "Hello World" << std::endl;
+int main () {
 
-    return 0;
+	ofstream Results;
+	ifstream Users;
+	ifstream Cars;
+	Cars.open ("CarsTable.csv");
+	//Make all of the vectors
+	vector<vector<string>> cars = readTable (Cars);
+	Cars.close();
+	Users.open ("UsersTable.csv");
+	vector<vector<string>> users = readTable (Users);
+	Users.close();
+	vector<vector<string>> join = innerJoin (users, cars);
+
+//	for(int i = 0; i < cars.size(); i++){
+//		cout << cars.at(i).at(0);
+//		for (int x = 1; x < cars.at(i).size(); x++){
+//			cout << "," << cars.at(i).at(x);
+//		}
+//		cout << endl;
+//	}
+	Results.open ("Results.csv");
+	//Print out the results
+	writeTable (Results, join);
+	Results.close();
+	return 0;
 }
