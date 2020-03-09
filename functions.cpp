e0bea9d95a95667cfb2d9ab403034401216613cf
Gouldilocks <Gouldilocks@users.noreply.github.com>
2020-03-09 17:12:55 -0500

    Added more comments for clarity's sake.

--- functions.cpp
+++ functions.cpp
@@ -24,28 +24,28 @@ vector<vector<string>> readTable (ifstream &file) {
 	return returnMe;
 }
 
-void writeTable (ofstream &file, vector<vector<string>> &table) {
-	for (int i = 0; i < table.size (); i++) {
-		file << table.at (i).at (0);
-		for (int x = 1; x < table.at (i).size (); x++) {
-			file << "," << table.at (i).at (x);
+void writeTable (ofstream &file, vector<vector<string>> &table) { // writes the table out
+	for (int i = 0; i < table.size (); i++) { // for loop to loop through the first vector array
+		file << table.at (i).at (0); // prints out first value, so that commas can be added later for formatting
+		for (int x = 1; x < table.at (i).size (); x++) { // for loop to loop through the data in the vector
+			file << "," << table.at (i).at (x); // prints out the rest of the values, with commas before each
 		}
-		file << endl;
+		file << endl; // ends line, and flushes.
 	}
 }
-vector<vector<string>> innerJoin (vector<vector<string>> leftTable, vector<vector<string>> rightTable) {
-	vector <string> toAdd;
-	vector <vector<string>> returnMe;
+vector<vector<string>> innerJoin (vector<vector<string>> leftTable, vector<vector<string>> rightTable) { // does the inner join.
+	vector <string> toAdd; // this string will be added to returnMe for each iteration.
+	vector <vector<string>> returnMe; // this is the vector that will be returned.
 	for (int z = 0; z < rightTable.size (); z++) { // these two for loops will iterate through every element of both vectors
 		for (int i = 0; i < leftTable.size (); i++) {
 			if (leftTable.at (i).at(0) == rightTable.at(z).at(0)){ // if the id's are the same, do this.
 
-				toAdd = leftTable.at(i);
-				for (int x = 0; x < rightTable.at(z).size(); x++){
+				toAdd = leftTable.at(i); // makes toAdd equivalent to the left table values at this index [i]
+				for (int x = 0; x < rightTable.at(z).size(); x++){ // this for loop will add each of the rightTable values to the array.
 						toAdd.push_back(rightTable.at(z).at(x));
 					}
-				toAdd.erase(toAdd.begin()+leftTable.at(1).size());
-				returnMe.push_back(toAdd);
+				toAdd.erase(toAdd.begin()+leftTable.at(1).size()); // this erases the data value that holds the second id value that comes from rightTable.
+				returnMe.push_back(toAdd); // puts this vector into the returnMe vector.
 
 			}
 		}
